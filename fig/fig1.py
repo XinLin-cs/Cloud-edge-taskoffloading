2d79075436af4a06366b7bc463a1d79d96caa670
ArComet <44643165+ArComet@users.noreply.github.com>
2022-05-28 22:39:35 +0800

    fig update

--- fig/fig1.py
+++ fig/fig1.py
@@ -7,19 +7,17 @@ df = pd.read_csv('./data/result/result.csv')
 XLabel = ['dataset1', 'dataset2', 'dataset3']
 x = np.arange(len(XLabel))
 
-homo = ['dataset1_homo', 'dataset2_homo', 'dataset3_homo']
-
-
-Cost_VMC = df[df['Tag'].isin(homo)][df['Method']=='VMC']['Cost'].tolist()
-Cost_HETO = df[df['Tag'].isin(homo)][df['Method']=='HETO']['Cost'].tolist()
-Cost_GREE = df[df['Tag'].isin(homo)][df['Method']=='GREE']['Cost'].tolist()
+Cost_HETO = df[df['Tag'].isin(XLabel)][df['Method']=='HETO']['Cost'].tolist()
+Cost_GREE = df[df['Tag'].isin(XLabel)][df['Method']=='GREE']['Cost'].tolist()
+Cost_D2D = df[df['Tag'].isin(XLabel)][df['Method']=='D2D']['Cost'].tolist()
 
 width = 0.10
+gap = 0.01
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width, Cost_GREE, width, label='GREE')
-rects2 = ax.bar(x , Cost_HETO, width, label='HETO')
-rects3 = ax.bar(x + width, Cost_VMC, width, label='VMC')
+rects3 = ax.bar(x - width - gap, Cost_D2D, width, label='D2D', hatch='/')
+rects2 = ax.bar(x , Cost_HETO, width, label='HETO', hatch='//')
+rects1 = ax.bar(x + width + gap, Cost_GREE, width, label='GREE', hatch='-')
 # 为y轴、标题和x轴等添加一些文本。
 ax.set_ylabel('Cost', fontsize=16)
 # ax.set_xlabel('X轴', fontsize=16)
--- fig/fig4.py
+++ fig/fig4.py
@@ -4,20 +4,21 @@ import numpy as np
 
 df = pd.read_csv('./data/result/result.csv')
 
-XLabel = ['dense1', 'sparse1']
+XLabel = ['dense1_homo', 'sparse1_homo']
 x = np.arange(len(XLabel))
 
+
 Cost_D2D = df[df['Tag'].isin(XLabel)][df['Method']=='D2D']['Cost'].tolist()
-Cost_HETO = df[df['Tag'].isin(XLabel)][df['Method']=='HETO']['Cost'].tolist()
 Cost_GREE = df[df['Tag'].isin(XLabel)][df['Method']=='GREE']['Cost'].tolist()
+Cost_VMC = df[df['Tag'].isin(XLabel)][df['Method']=='VMC']['Cost'].tolist()
 
 width = 0.10
 gap = 0.01
 
 fig, ax = plt.subplots()
 rects1 = ax.bar(x - width - gap, Cost_D2D, width, label='D2D', hatch='/')
-rects2 = ax.bar(x , Cost_HETO, width, label='HETO', hatch='//')
-rects3 = ax.bar(x + width + gap, Cost_GREE, width, label='GREE', hatch='-')
+rects2 = ax.bar(x , Cost_GREE, width, label='GREE', hatch='//')
+rects3 = ax.bar(x + width + gap, Cost_VMC, width, label='VMC', hatch='-')
 # 为y轴、标题和x轴等添加一些文本。
 ax.set_ylabel('Cost', fontsize=16)
 # ax.set_xlabel('X轴', fontsize=16)
